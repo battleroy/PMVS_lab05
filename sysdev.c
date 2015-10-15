863d8d051588a164c8a90ab1d639f659e0f45a10
battleroy <cnam.sux@gmail.com>
2015-10-16 00:47:11 +0300

    Source added

---
+++ sysdev.c
@@ -0,0 +1,91 @@
+#include <linux/kobject.h>
+#include <linux/string.h>
+#include <linux/sysfs.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/timer.h>
+
+static int timer = 0;
+static struct timer_list my_timer;
+
+void my_timer_callback(unsigned long data)
+{
+	printk("Hello, World! Jiffies: %ld\n", jiffies);
+	my_timer.expires += msecs_to_jiffies(timer * 1000);
+	add_timer(&my_timer);
+}
+
+static ssize_t timer_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "Timer: %d\n", timer);
+}
+
+static ssize_t timer_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int retval;
+
+	printk(KERN_ALERT "Store called\n");
+	sscanf(buf, "%du", &timer);
+
+	if (timer_pending(&my_timer)) {
+		retval = del_timer_sync(&my_timer);
+		if (retval)
+			printk("The timer is still in use...\n");
+	}
+
+	if (timer > 0) {
+		my_timer.function = my_timer_callback;
+		my_timer.expires = jiffies + msecs_to_jiffies(timer * 1000);
+		add_timer(&my_timer);
+	}
+
+	printk("count: %d\n", (int)count);
+	return count;
+}
+
+static struct kobj_attribute timer_attribute =
+	__ATTR(timer, 0665, timer_show, timer_store);
+
+static struct attribute *attrs[] = {
+	&timer_attribute.attr,
+	NULL,
+};
+
+static struct attribute_group attr_group = {
+	.attrs = attrs,
+};
+
+static struct kobject *timer_kobj;
+
+static int __init timer_init(void)
+{
+	int retval;
+
+	timer_kobj = kobject_create_and_add("dev_timer", kernel_kobj);
+	if (!timer_kobj)
+		return -ENOMEM;
+
+	retval = sysfs_create_group(timer_kobj, &attr_group);
+	if (retval)
+		kobject_put(timer_kobj);
+
+	init_timer(&my_timer);
+
+	return retval;
+}
+
+static void __exit timer_exit(void)
+{
+	if (timer_pending(&my_timer)) {
+		int retval = del_timer_sync(&my_timer);
+		if (retval)
+			printk("The timer is still in use...\n");
+	}
+
+	kobject_put(timer_kobj);
+}
+
+module_init(timer_init);
+module_exit(timer_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("/~~GeNeRaL~~/ \\o.");
